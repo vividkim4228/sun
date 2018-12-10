98d089a8b8709483e7e7b2ef3c1524bc1fc5ed66
vividkim <vividkim@gmail.com>
2018-12-10 17:47:46 +0900

    181210 end

--- b_example/c_normal/js/src/inner_content_01.js
+++ b_example/c_normal/js/src/inner_content_01.js
@@ -47,7 +47,244 @@ var viewBox = $('#viewBox');
 viewBox.before('<div class="view view-before"></div>');
 viewBox.after('<div class="view view-after"></div>');
 $('.view').css({width:'100%', height:'1rem',  backgroundColor:'#f0a'})
-console.log(viewBox.prev());
-console.log(viewBox.next());
+// console.log(viewBox.prev());
+// console.log(viewBox.next());
+
+
+// ===== 월요일 수업내용 ======================================================
+
+var headBox = $('#headBox');
+  // headBox.children('h1').after('<nav id="gnb"></nav>');
+  headBox.find('.head_wrap').append('<nav id="gnb"></nav>');
+  var gnb = $('#gnb');
+  // html()은 기존의 내용을 모두 삭제하고 새로 쓴다.
+  gnb.html('<h2>navigation</h2>');
+
+
+ gnb.append('<ul></ul>');
+ var gnbUl = gnb.children('ul');
+ // li를 5개 만드시오.
+ // 방법 1- 직접 작성하기
+   // gnbUl.append('<li><a href="http://google.com">list_01</a></li>');
+   // gnbUl.append('<li><a href="http://naver.com">list_02</a></li>');
+   // gnbUl.append('<li><a href="http://daum.net">list_03</a></li>');
+   // gnbUl.append('<li><a href="http://w3schools.com">list_04</a></li>');
+   // gnbUl.append('<li><a href="http://caniuse.com">list_05</a></li>');
+
+ // 방법 2- 반복문사용하기
+  // var i = 0; var j;
+  // for(; i<5; i+=1){
+  //   j= i + 1; // 숫자를 0부터가 아닌 1부터 작성하도록 처리
+  //   gnbUl.append('<li><a href="#">list_0'+ j +'</a></li>');
+  // }
+
+ // 덧1 - 링크주소 별도로 만들어 담기(배열사용)
+  var i = 0; var j;
+  var addr = [ 'http://google.com',
+               'http://naver.com',
+               'http://daum.net',
+               'http://w3schools.com',
+               'http://caniuse.com',
+               'http://kakao.com'
+              ];
+
+  // for(; i < addr.length; i+=1){
+  //   j= i + 1;
+  //   gnbUl.append('<li><a href="' + addr[i] + '">list_0'+ j +'</a></li>');
+  // }
+
+  for(i = addr.length; i > 0 ; i-=1){
+    j= i - 1;
+    gnbUl.prepend('<li><a href="' + addr[j] + '">list_0'+ i +'</a></li>');
+  }
+
+  var head = $('head');
+  head.append('<style></style>');
+  var style = head.children('style');
+
+  style.append('\
+      #gnb{width:100%; height:auto; padding:0.5rem 1rem; box-sizing:border-box;}\
+      #gnb>ul{width:100%; height:auto;}\
+      #gnb>ul:after{content:" "; display:block; clear:both;}\
+      #gnb li{width:100px; height:auto; float:left; margin-right:1rem; background-color:#fff; text-align:center;}\
+      ');
+
+// var conBox = $('#conBox');
+
+// conBox.append('<div class = "content1"<h2>content1</h2></div>');
+// conBox.append('<div class = "content2"<h2>content2</h2></div>');
+// conBox.append('<div class = "content3"<h2>content3</h2></div>');
+// conBox.empty();
+// conBox.remove();
+
+
+var foot = $('#footBox');
+/* //** appendTo , prependTo 기능이해하기
+  var to1 = '<p>내용에 이전에 삽입하고 있음</p>';
+  // 방에 아버지께서 들어가신다.
+  // foot.prepend(to1);
+
+  // 아버지께서 방에 들어가신다.
+  $(to1).prependTo(foot);
+
+  var to2 = '<p>내용에 뒤에 삽입하여 처리 </p>';
+  // foot.append(to2);
+  $(to2).appendTo(foot);
+*/
+ foot.css({height:'auto'});
+ foot.append('<ul></ul>');
+var footUl = foot.children('ul');
+footUl.css({margin:'1rem auto', width:'70%', height:'30rem', backgroundColor:'#faf', boxSizing:'border-box', padding:'1rem'});
+
+//  --------------------------------------
+// footUl.append('<li>list_01</li>');
+// footUl.append('<li>list_02</li>');
+// footUl.append('<li>list_03</li>');
+// footUl.append('<li>list_04</li>');
+// footUl.append('<li>list_05</li>');
+
+// footUl.prepend('<li>list_01</li>');
+// footUl.prepend('<li>list_02</li>');
+// footUl.prepend('<li>list_03</li>');
+
+
+var addr2 = ['a', 'b', 'c', 'd'];
+// console.log( addr2[3] );
+// console.log( addr2.length );
+var addLen = addr2.length;
+
+
+// for(var k = 0; k<addLen; k+=1){
+//  // console.log(k);
+//  // console.log( addr2[k] );
+//  footUl.append('<li><a href="' + addr2[k] + '">list_'+ (k+1) +'</a></li>');
+// }
+
+for(var k = addLen; k > 0; k-=1){
+ // console.log(k);
+ // console.log( addr2[k-1] );
+ footUl.prepend('<li><a href="'+ addr2[k-1] + '"> list_'+ k +'</a></li>');
+}
+
+
+foot.find('li').css({width:'100%', height:'2rem', marginBottom:'0.5rem', fontSize:'1.3rem', fontWeight:'bold', textAlign:'center', backgroundColor:'#fff'});
+
+var footBox = $('#footBox');
+var footLi = footBox.find('li');
+// jQuery에서는 a 를 li의 모든요소에서 찾아낸다.
+// js에서는 li의 순서를 명확하게 인지한 후에 찾아내야 한다.
+// 단, li의 명확한 순서에 맞는 자식 a 에게 기능을 부여하려면 li에서 정확하게 순서를 지칭해야한다.
+
+// var fb = document.getElementById('footBox');
+// var fbUl = fb.children[1];
+// var fbLi - fbUl.children;
+// for (var l=0; l<fbLi.length; l+=1){console.log(fbLi[l].children);}
+
+
+// var footA = footLi.eq(0).children('a');
+// console.log(footA);
+// var faa = footA.attr('href');
+// console.log(faa);
+
+// var headId = $('header').attr('id');
+// console.log(headId);
+
+// var wc = $('#headBox').children().attr('class');
+// console.log(wc)
+
+var lsBox = $('#listBox').children('ul');
+// console.log(lsBox);
+var lsLista = lsBox.children('li').eq(3).find('a');
+// []선택은 값 자체만을 확인하는것.
+// eq선택은 이후에 다른 메소드 기능을 수행할수 있음.
+// console.log(lsLista.attr('href'));
+var re_lsLista = lsLista.attr({'href':'http://naver.com',
+                               'target':'_blank', 
+                               'class':'more'});
+
+// a요소의 속성 target 
+// _self 기본
+// _blank 새창에서 열기
+// 
+
+
+// 아래의 두 방식은 같은것이다. 
+
+// $('a').css('color','#fc0');
+// $('a').css('fontSize','1.3rem');
+// $('a').css('margin','0.5rem');
+
+// $('a').css({color:'#fc0', fontSize:'1.3rem', margin:'0.5rem'});
+
+$('#listBox').css({backgroundColor:'#fc0',width:'90%', margin:'auto'});
+
+
+console.log(re_lsLista);
+
+
+lsLista.find('li').removeAttr('class');
+
+// varl()
+
+var inputId = $('#input_1').val();
+console.log(inputId);
+
+var input2 = $('#input_2').on('keyup',function(){
+  var myText = $(this).val();
+  console.log(myText);
+});
+
+// clone() ================================
+
+// var product = $('.shop').children().eq(0).clone(true); 
+// shop.append(product);
+// 복잡한기능까지 사용하려면 'true' 작성
+// 필요한 갯수만큼 복제한 후 마지막자식은 지우기 
+// var color =['#fc0','#cca','#acf','#f06','#f7f','#afa'];
+// var link =['http://naver.com','http://daum.net','http://google.com','http://behance.com','http://pinterest.com','http://dribbble.com'];
+// var text = ['제품1', 
+// '제품2', 
+//   '제품3', 
+//     '제품4', 
+//   '제품5', 
+//         '제품6'];
+
+var productList = [
+  {color:'#fc0', text:'제품01', link:'http://naver.com'},
+  {color:'#cca', text:'제품02', link:'http://daum.net'},
+  {color:'#acf', text:'제품03', link:'http://google.com'},
+  {color:'#f06', text:'제품04', link:'http://behance.com'},
+  {color:'#f7f', text:'제품05', link:'http://pinterest.com'},
+  {color:'#afa', text:'제품06', link:'http://dribbble.com'}
+];
+
+var shop = $('.shop');
+var product ,prEq
+for(var i = 0; i<productList.length; i+=1){
+product= $('.product').eq(0).clone(true); 
+shop.append(product);
+// $('.product').eq(i).find('.shop_img').css({backgroundColor:color[i]});
+// $('.product').eq(i).find('p').eq(0).text(text[i]);
+// $('.product').eq(i).find('a').attr('href',link[i]);
+
+prEq=$('.product').eq(i)
+
+prEq.find('.shop_img').css({backgroundColor:productList[i].color});
+prEq.find('p').eq(0).text(productList[i].text);
+prEq.find('a').attr('href',productList[i].link);
+}
+
+// var len = $('.product').length;
+// console.log(len);
+$('.product').eq(-1).remove();
+
+
+
+
+
+
+
+
+
 
 })(jQuery);
