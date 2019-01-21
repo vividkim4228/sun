08766d465d1d480636a659d1f1171b6871f22c68
vividkim4228 <vividkim4228@gmail.com>
2019-01-22 01:44:39 +0900

    home

--- c_portfolio/js/src/gillette/common/gillette_header.js
+++ c_portfolio/js/src/gillette/common/gillette_header.js
@@ -3,12 +3,17 @@
 (function($){
   var menu = $('.menu');
   var menuTitle = menu.find('dt');
+  var dd = menu.find('dd');
+
   menuTitle.on('mouseenter',function(e){
     menu.addClass('menu_active');
+    dd.slideDown();
   });
 
   menuTitle.on('mouseleave',function(e){
     menu.removeClass('menu_active');
+		dd.css({display:'none'});
+
   });
 
 })(jQuery);
